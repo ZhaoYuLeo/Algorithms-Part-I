6fccda7b6a01663c5fc0a7e4d6ca9ff1893d1d66
ZhaoYuLeo <zhaoyuleoo@gmail.com>
2023-04-23 00:01:49 +0800

    feat: recursive ln(N!\)

---
+++ 1_fundamentals/1_1_ProgModel/Ex_20.java
@@ -0,0 +1,15 @@
+public class Ex_20 {
+    public static double ln(int N) {
+        if (N <= 0) {
+            return 0;
+        } else {
+            return Math.log(N) + ln(N - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("ln(1!) is: " + ln(1));
+        System.out.println("ln(0!) is: " + ln(0));
+        System.out.println("ln(10) is: " + ln(10));
+    }
+}
