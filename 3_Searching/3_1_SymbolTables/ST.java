89847003c7e26dfc839e30d0faebe75f0edef7bf
ZhaoYuLeo <zhaoyuleoo@gmail.com>
2023-05-04 23:58:00 +0800

    feat: interface for symbol tables

---
+++ 3_Searching/3_1_SymbolTables/ST.java
@@ -0,0 +1,26 @@
+/* *****************************************************************************
+ *  Name:              Alan Turing
+ *  Coursera User ID:  123456
+ *  Last modified:     1/1/2019
+ **************************************************************************** */
+
+/**
+ * @param <Key>
+ * @param <Value>
+ */
+public interface ST<Key, Value> {
+
+    void put(Key key, Value val);
+
+    Value get(Key key);
+
+    void delete(Key key);
+
+    boolean contains(Key key);
+
+    boolean isEmpty();
+
+    int size();
+
+    Iterable<Key> keys();
+}
