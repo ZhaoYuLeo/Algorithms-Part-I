e8e9d2b475b940fca728c3aefa8b41ceed2e99d3
ZhaoYuLeo <zhaoyuleoo@gmail.com>
2023-05-29 12:44:45 +0800

    docs: mystery pow and product

--- chapter1/section1/Ex_18.java
+++ chapter1/section1/Ex_18.java
@@ -4,6 +4,7 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class Ex_18 {
     // product
+    // The number of times a is added is b in binary form
     public static int mystery(int a, int b) {
         if (b == 0) return 0;
         if (b % 2 == 0) return mystery(a+a, b/2);
@@ -11,6 +12,7 @@ public class Ex_18 {
     }
 
     // pow
+    // The number of times a is multiplied is b in binary form
     public static int mystery1(int a, int b) {
         if (b == 0) return 1;
         if (b % 2 == 0) return mystery1(a*a, b/2);
