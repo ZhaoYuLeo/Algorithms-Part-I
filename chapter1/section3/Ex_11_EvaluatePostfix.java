30ce2e7644f8bb5c8afe0487dd495ba544f29410
ZhaoYuLeo <zhaoyuleoo@gmail.com>
2023-06-11 12:08:26 +0800

    fix: modify the form of the postfix

--- chapter1/section3/Ex_11_EvaluatePostfix.java
+++ chapter1/section3/Ex_11_EvaluatePostfix.java
@@ -5,34 +5,36 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class Ex_11_EvaluatePostfix {
     public static int evaluatePostfix(String postfix) {
-        String[] expr = postfix.replaceAll("\\s+", "").split("\\s+");
+        // Assume postfix is an expression without parentheses
+        String[] expr = postfix.split("\\s+");
 
-        Stack<String> ops = new Stack<>();
         Stack<Integer> vals = new Stack<>();
         for (String e : expr) {
-            if (e.equals("(")) {
-            } else if (e.equals("+")
+            if (e.equals("+")
                         || e.equals("-")
                         || e.equals("*")
                         || e.equals("/")
                         || e.equals("sqrt")) {
-                ops.push(e);
-            } else if (e.equals(")")) {
-                if (vals.isEmpty() || ops.isEmpty()) {
+                if (vals.isEmpty()) {
                     throw new IllegalArgumentException("The postfix expression is not what we expected.");
                 }
-                String op = ops.pop();
-                Integer v = vals.pop();
-                if (op.equals("sqrt")) {
-                    vals.push((int)Math.sqrt(v));
-                } else if (op.equals("+")) {
-                    vals.push(vals.pop() + v);
-                } else if (op.equals("-")) {
-                    vals.push(vals.pop() - v);
-                } else if (op.equals("*")) {
-                    vals.push(vals.pop() * v);
-                } else if (op.equals("/")) {
-                    vals.push(vals.pop() / v);
+                Integer v2 = vals.pop();
+                if (e.equals("sqrt")) {
+                    vals.push((int)Math.sqrt(v2));
+                    continue;
+                }
+                if (vals.isEmpty()) {
+                    throw new IllegalArgumentException("The postfix expression is not what we expected.");
+                }
+                Integer v1 = vals.pop();
+                if (e.equals("+")) {
+                    vals.push(v1 + v2);
+                } else if (e.equals("-")) {
+                    vals.push(v1 - v2);
+                } else if (e.equals("*")) {
+                    vals.push(v1 * v2);
+                } else if (e.equals("/")) {
+                    vals.push(v1 / v2);
                 }
             } else {
                 vals.push(Integer.parseInt(e));
@@ -43,7 +45,7 @@ public class Ex_11_EvaluatePostfix {
     }
 
     public static void main(String[] args) {
-        String postfix = "(1 ((2 3 +) (4 5 *) *) +)";
+        String postfix = "200 2 / sqrt 2 3 + 4 15 * * +";
         StdOut.println(evaluatePostfix(postfix));
     }
 }
