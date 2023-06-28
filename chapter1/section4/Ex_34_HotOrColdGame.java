7a73a09d03fe195547a74ee2f5b75d6a7e6dc875
ZhaoYuLeo <zhaoyuleoo@gmail.com>
2023-06-28 18:20:56 +0800

    feat: add exercise 1.4.34 lg N

--- chapter1/section4/Ex_34_HotOrColdGame.java
+++ chapter1/section4/Ex_34_HotOrColdGame.java
@@ -28,7 +28,7 @@ public class Ex_34_HotOrColdGame {
     }
 
     public void setSecret(int secret) {
-        // given secret may not exist between 1 and N.
+        // The given secret may not exist between 1 and N.
         this.secret = secret;
     }
 
@@ -40,20 +40,59 @@ public class Ex_34_HotOrColdGame {
     private int findSecret(int preGuess, int curGuess) {
         // We will only check the right border.
         // This step should be considered as two guesses.
+        // The last gusess can never be reused in worst case.
+        // 1(4)------5(8)---7--3(6)-------------2
         Result r = check(preGuess, curGuess);
-        this.countGuess += 2;
         if (r == Result.EQUAL) {
             return curGuess;
         }
         if (r == Result.HOTTER) {
             // (a + b) / 2 may cause overflow
-            return findSecret(preGuess + (curGuess - preGuess) / 2 + 1, curGuess);
+            this.countGuess += 1;
+            return findSecret(curGuess, preGuess + (curGuess - preGuess) / 2 + 1);
         }
         if (r == Result.COLDER) {
+            this.countGuess += 2;
             return findSecret(preGuess, preGuess + (curGuess - preGuess) / 2);
         }
         if (r == Result.SAME && preGuess != curGuess) {
             return preGuess + (curGuess - preGuess) / 2;
+        }
+        return -1;
+    }
+
+    private int findSecretLgN() {
+        this.countGuess = 0;
+        return findSecretLgN(1, N, 1, N);
+    }
+
+    /**
+     * Our purpose is to divide the area by two after each guess we made.
+     * 3*******1|----------------|2**4
+     *          |--------|
+     *               |---|
+     * ~lg N + 1
+     */
+    private int findSecretLgN(int lo, int hi, int preGuess, int curGuess) {
+        int mid = lo + (hi - lo) / 2;
+        Result r = check(preGuess, curGuess);
+        if (r == Result.EQUAL) {
+            return curGuess;
+        }
+        if (r == Result.SAME && preGuess != curGuess) {
+            // the secret is the split point
+            return preGuess + (curGuess - preGuess) / 2;
+        }
+        if (lo >= hi) {
+            return -1;
+        }
+        if (r == Result.HOTTER && curGuess > mid || r == Result.COLDER && curGuess < mid) {
+            this.countGuess += 1;
+            return findSecretLgN(mid + 1, hi, curGuess, hi + mid + 1 - curGuess);
+        }
+        if (r == Result.COLDER && curGuess > mid || r == Result.HOTTER && curGuess < mid) {
+            this.countGuess += 1;
+            return findSecretLgN(lo, mid, curGuess, lo + mid - curGuess); 
         }
         return -1;
     }
@@ -85,6 +124,11 @@ public class Ex_34_HotOrColdGame {
         return this.countGuess;
     }
 
+    public double countTrialLgN() {
+        checkResult(findSecretLgN());
+        return this.countGuess;
+    }
+
     public double timeTrial() {
         // Time findSecret()
         Stopwatch timer = new Stopwatch();
@@ -106,6 +150,24 @@ public class Ex_34_HotOrColdGame {
         }
     }
 
+     public static void draw(double[] xs, double[] ys, double[] ys2) {
+        // Assume we get two sorted array.
+        for (int i = 0; i < xs.length; i += 1) {
+            // N -> lg N
+            xs[i] = (Math.log(xs[i]) / Math.log(2));
+        }
+        StdDraw.setXscale(xs[0] - 1, xs[xs.length - 1] + 1);
+        StdDraw.setYscale(Math.min(ys[0], ys2[0]) - 1, 
+                          Math.max(ys[ys.length - 1], ys[ys2.length - 1]) + 1);
+        StdDraw.setPenRadius(0.02);
+        for (int i = 0; i< xs.length; i += 1) {
+            StdDraw.setPenColor(StdDraw.BLACK);
+            StdDraw.point(xs[i], ys[i]);
+            StdDraw.setPenColor(StdDraw.BLUE);
+            StdDraw.point(xs[i], ys2[i]);
+        }
+    }
+
     public static void main(String[] args) {
         // set scale
         int MAXSCALE = 1000000;
@@ -114,19 +176,23 @@ public class Ex_34_HotOrColdGame {
         // run test
         double[] results = new double[(int)(Math.log(MAXSCALE / INTERVAL) / Math.log(2)) + 1];
         double[] ns = new double[results.length];
+        double[] results2 = new double[results.length];
         int i = 0;
         for (int N = INTERVAL; N <= MAXSCALE; N += N) {
             Ex_34_HotOrColdGame g = new Ex_34_HotOrColdGame(N);
             double totalGuess = 0;
+            double totalGuess2 = 0;
             // double totalTime = 0;
             for (int c = 0; c < COUNT ; c += 1) {
                 // Argument StdRandom.uniform can take is less than Integer.MAX_VALUE
                 g.setSecret(StdRandom.uniform(1, N + 1));
                 totalGuess += g.countTrial();
+                totalGuess2 += g.countTrialLgN();
                 // totalTime += g.timeTrial();
             }
             ns[i] = N;
-            results[i++] = totalGuess / COUNT;
+            results[i] = totalGuess / COUNT;
+            results2[i++] = totalGuess2 / COUNT;
             // results[i++] = totalTime;
         }
         // show result
@@ -134,7 +200,7 @@ public class Ex_34_HotOrColdGame {
             StdOut.println(r);
         }
         // draw plot
-        Ex_34_HotOrColdGame.draw(ns, results);
+        Ex_34_HotOrColdGame.draw(ns, results, results2);
 
         // // draw plot(result of one experiment) 
         // int MAXSCALE = 1000000000;
