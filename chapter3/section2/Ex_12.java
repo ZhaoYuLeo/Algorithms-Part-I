1ff19d8784a3fe94019dee7b75e4533378632f22
ZhaoYuLeo <zhaoyuleoo@gmail.com>
2023-10-10 22:25:53 +0800

    feat: 3.2.12

---
+++ chapter3/section2/Ex_12.java
@@ -0,0 +1,266 @@
+package chapter3.section2;
+
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Develop a BST implementation that omits rank() and select() and does not use a count field in Node.
+ */
+public class Ex_12<Key extends Comparable<Key>, Value> {
+    private Node root;              // root of BST
+    private int size;               // size of BST
+
+    private class Node {
+        private Key key;            // key
+        private Value val;          // associated value
+        private Node left, right;   // links to subtrees
+
+        public Node(Key key, Value val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Value get(Key key) {
+        return get(root, key);
+    }
+
+    private Value get(Node x, Key key) {
+        // Return value associated with key in the subtree rooted at x;
+        // return null if key not present in subtree rooted at x.
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return get(x.left, key);
+        } else if (cmp > 0) {
+            return get(x.right, key);
+        } else {
+            return x.val;
+        }
+    }
+
+    public void put(Key key, Value val) {
+        // Search for key. Update value if found; grow table if new.
+        root = put(root, key, val);
+    }
+
+    private Node put(Node x, Key key, Value val) {
+        // Change key's value to val if key in subtree rooted at x.
+        // Otherwise, add new node to subtree associating key with val.
+        if (x == null) {
+            this.size += 1;
+            return new Node(key, val);
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = put(x.left, key, val);
+        } else if (cmp > 0) {
+            x.right = put(x.right, key, val);
+        } else {
+            x.val = val;
+        }
+        return x;
+    }
+
+    public Key min() {
+        return min(root).key;
+    }
+
+    private Node min(Node x) {
+        if (x.left == null) {
+            return x;
+        }
+        return min(x.left);
+    }
+
+    public Key max() {
+        return max(root).key;
+    }
+
+    private Node max(Node x) {
+        if (x.right == null) {
+            return x;
+        }
+        return max(x.right);
+    }
+
+    public Key floor(Key key) {
+        Node x = floor(root, key);
+        if (x == null) {
+            return null;
+        }
+        return x.key;
+    }
+
+    private Node floor(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) {
+            return x;
+        }
+        if (cmp < 0) {
+            return floor(x.left, key);
+        }
+        Node t = floor(x.right, key);
+        if (t != null) {
+            return t;
+        } else {
+            return x;
+        }
+    }
+
+    public Key ceiling(Key key) {
+        Node x = ceiling(root, key);
+        if (x == null) {
+            return null;
+        }
+        return x.key;
+    }
+
+    private Node ceiling(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) {
+            return x;
+        }
+        if (cmp > 0) {
+            return ceiling(x.right, key);
+        }
+        Node t = ceiling(x.left, key);
+        if (t != null) {
+            return t;
+        } else {
+            return x;
+        }
+    }
+
+    public void deleteMin() {
+        root = deleteMin(root);
+    }
+
+    private Node deleteMin(Node x) {
+        if (x.left == null) {
+            this.size -= 1;
+            return x.right;
+        }
+        x.left = deleteMin(x.left);
+        return x;
+    }
+
+    public void deleteMax() {
+        root = deleteMax(root);
+    }
+
+    private Node deleteMax(Node x) {
+        if (x.right == null) {
+            this.size -= 1;
+            return x.left;
+        }
+        x.right = deleteMax(x.right);
+        return x;
+    }
+
+    public void delete(Key key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left  = delete(x.left,  key);
+        } else if (cmp > 0) {
+            x.right = delete(x.right, key);
+        } else {
+            this.size -= 1;
+            if (x.right == null) {
+                return x.left;
+            }
+            if (x.left == null) {
+                return x.right;
+            }
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        return x;
+    }
+
+    public Iterable<Key> keys() {
+        return keys(min(), max());
+    }
+
+    public Iterable<Key> keys(Key lo, Key hi) {
+        Queue<Key> queue = new Queue<Key>();
+        keys(root, queue, lo, hi);
+        return queue;
+    }
+
+    private void keys(Node x, Queue<Key> queue, Key lo, Key hi) {
+        if (x == null) {
+            return;
+        }
+        int cmplo = lo.compareTo(x.key);
+        int cmphi = hi.compareTo(x.key);
+        if (cmplo < 0) {
+            keys(x.left, queue, lo, hi);
+        }
+        if (cmplo <= 0 && cmphi >= 0) {
+            queue.enqueue(x.key);
+        }
+        if (cmphi > 0) {
+            keys(x.right, queue, lo, hi);
+        }
+    }
+
+    public Iterable<Key> levelOrder() {
+        // Returns the keys in the BST in level order (for debugging)
+        Queue<Key> keys = new Queue<Key>();
+        // record in stack if we write this method in a recursive way.
+        Queue<Node> queue = new Queue<Node>();
+        queue.enqueue(root);
+        while (!queue.isEmpty()) {
+            Node x = queue.dequeue();
+            if (x == null) {
+                continue;
+            }
+            keys.enqueue(x.key);
+            queue.enqueue(x.left);
+            queue.enqueue(x.right);
+        }
+        return keys;
+    }
+
+    public static void main(String[] args) {
+        Ex_07_BST<String, Integer> st = new Ex_07_BST<String, Integer>();
+        for (int i = 0; !StdIn.isEmpty(); i += 1) {
+            String key = StdIn.readString();
+            st.put(key, i);
+        }
+
+        for (String s : st.levelOrder()) {
+            StdOut.println(s + " " + st.get(s));
+        }
+
+        StdOut.println(st.size());
+
+        for (String s : st.keys()) {
+            StdOut.println(s + " " + st.get(s));
+        }
+
+    }
+}
