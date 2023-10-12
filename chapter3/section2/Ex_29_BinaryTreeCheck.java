b268222e08a98e8a1f91098512079836a34faea8
ZhaoYuLeo <zhaoyuleoo@gmail.com>
2023-10-13 01:59:53 +0800

    feat: add 3.2.29

---
+++ chapter3/section2/Ex_29_BinaryTreeCheck.java
@@ -0,0 +1,348 @@
+package chapter3.section2;
+
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Write a recursive method isBinaryTree() that takes a Node as argument and returns true if the
+ * subtree count field N is consistent in the data structure rooted at that node, false otherwise.
+ * Note: This check also ensures that the data structure has no cycles and is therefore a binary
+ * tree(!)
+ */
+public class Ex_29_BinaryTreeCheck<Key extends Comparable<Key>, Value> {
+    private Node root;              // root of BST
+
+    private class Node {
+        private Key key;            // key
+        private Value val;          // associated value
+        private Node left, right;   // links to subtrees
+        private int N;              // # nodes in subtree rooted here
+
+        public Node(Key key, Value val, int N) {
+            this.key = key;
+            this.val = val;
+            this.N = N;
+        }
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        if (x == null) {
+            return 0;
+        } else {
+            return x.N;
+        }
+    }
+
+    public Value get(Key key) {
+        return get(root, key);
+    }
+
+    private Value get(Node x, Key key) {
+        // Return value associated with key in the subtree rooted at x;
+        // return null if key not present in subtree rooted at x.
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+
+        // StdOut.println(x.key);
+
+        if (cmp < 0) {
+            return get(x.left, key);
+        } else if (cmp > 0) {
+            return get(x.right, key);
+        } else {
+            return x.val;
+        }
+    }
+
+    public void put(Key key, Value val) {
+        // Search for key. Update value if found; grow table if new.
+        root = put(root, key, val);
+    }
+
+    private Node put(Node x, Key key, Value val) {
+        // Change key's value to val if key in subtree rooted at x.
+        // Otherwise, add new node to subtree associating key with val.
+        if (x == null) return new Node(key, val, 1);
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = put(x.left, key, val);
+        } else if (cmp > 0) {
+            x.right = put(x.right, key, val);
+        } else {
+            x.val = val;
+        }
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public Key min() {
+        return min(root).key;
+    }
+
+    private Node min(Node x) {
+        if (x.left == null) {
+            return x;
+        }
+        return min(x.left);
+    }
+
+    public Key max() {
+        return max(root).key;
+    }
+
+    private Node max(Node x) {
+        if (x.right == null) {
+            return x;
+        }
+        return max(x.right);
+    }
+
+    public Key floor(Key key) {
+        Node x = floor(root, key);
+        if (x == null) {
+            return null;
+        }
+        return x.key;
+    }
+
+    private Node floor(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) {
+            return x;
+        }
+        if (cmp < 0) {
+            return floor(x.left, key);
+        }
+        Node t = floor(x.right, key);
+        if (t != null) {
+            return t;
+        } else {
+            return x;
+        }
+    }
+
+    public Key ceiling(Key key) {
+        Node x = ceiling(root, key);
+        if (x == null) {
+            return null;
+        }
+        return x.key;
+    }
+
+    private Node ceiling(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) {
+            return x;
+        }
+        if (cmp > 0) {
+            return ceiling(x.right, key);
+        }
+        Node t = ceiling(x.left, key);
+        if (t != null) {
+            return t;
+        } else {
+            return x;
+        }
+    }
+
+    public Key select(int k) {
+        return select(root, k).key;
+    }
+
+    private Node select(Node x, int k) {
+        // Return Node containing key of rank k.
+        if (x == null) {
+            return null;
+        }
+        int t = size(x.left);
+        if (t > k) {
+            return select(x.left, k);
+        } else if (t < k) {
+            return select(x.right, k - t - 1);
+        } else {
+            return x;
+        }
+    }
+
+    public int rank(Key key) {
+        return rank(key, root);
+    }
+
+    private int rank(Key key, Node x) {
+        // Return number of keys less than x.key in the subtree rooted at x.
+        if (x == null) {
+            return 0;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return rank(key, x.left);
+        } else if (cmp > 0) {
+            return 1 + size(x.left) + rank(key, x.right);
+        } else {
+            return size(x.left);
+        }
+    }
+
+
+    public void deleteMin() {
+        root = deleteMin(root);
+    }
+
+    private Node deleteMin(Node x) {
+        if (x.left == null) {
+            return x.right;
+        }
+        x.left = deleteMin(x.left);
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public void deleteMax() {
+        root = deleteMax(root);
+    }
+
+    private Node deleteMax(Node x) {
+        if (x.right == null) {
+            return x.left;
+        }
+        x.right = deleteMax(x.right);
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public void delete(Key key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left  = delete(x.left,  key);
+        } else if (cmp > 0) {
+            x.right = delete(x.right, key);
+        } else {
+            if (x.right == null) {
+                return x.left;
+            }
+            if (x.left == null) {
+                return x.right;
+            }
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public Iterable<Key> keys() {
+        return keys(min(), max());
+    }
+
+    public Iterable<Key> keys(Key lo, Key hi) {
+        Queue<Key> queue = new Queue<Key>();
+        keys(root, queue, lo, hi);
+        return queue;
+    }
+
+    private void keys(Node x, Queue<Key> queue, Key lo, Key hi) {
+        if (x == null) {
+            return;
+        }
+        int cmplo = lo.compareTo(x.key);
+        int cmphi = hi.compareTo(x.key);
+        if (cmplo < 0) {
+            keys(x.left, queue, lo, hi);
+        }
+        if (cmplo <= 0 && cmphi >= 0) {
+            queue.enqueue(x.key);
+        }
+        if (cmphi > 0) {
+            keys(x.right, queue, lo, hi);
+        }
+    }
+
+    public Iterable<Key> levelOrder() {
+        // Returns the keys in the BST in level order (for debugging)
+        Queue<Key> keys = new Queue<Key>();
+        Queue<Node> queue = new Queue<Node>();
+        queue.enqueue(root);
+        while (!queue.isEmpty()) {
+            Node x = queue.dequeue();
+            if (x == null) {
+                continue;
+            }
+            keys.enqueue(x.key);
+            queue.enqueue(x.left);
+            queue.enqueue(x.right);
+        }
+        return keys;
+    }
+
+    public boolean isBinaryTree2() {
+        return isBinaryTree2(root);
+    }
+
+    private boolean isBinaryTree2(Node x) {
+        if (x == null) {
+            return true;
+        }
+        // assume the size function is implemented correctly
+        if (size(x) != size(x.left) + size(x.right) + 1) {
+            return false;
+        }
+        return isBinaryTree2(x.left) && isBinaryTree2(x.right);
+    }
+
+    public boolean isBinaryTree() {
+        return isBinaryTree(root, 0, 0) == 0;
+    }
+
+    private int isBinaryTree(Node x, int count, int passed) {
+        // Add all Ns up and substract it  
+        if (x == null) {
+            return count;
+        }
+        passed += 1;
+        count += (x.N - passed);
+        return isBinaryTree(x.left, 0, passed) + isBinaryTree(x.right, count, passed);
+    }
+
+    public static void main(String[] args) {
+        Ex_29_BinaryTreeCheck<String, Integer> st = new Ex_29_BinaryTreeCheck<String, Integer>();
+        for (int i = 0; !StdIn.isEmpty(); i += 1) {
+            String key = StdIn.readString();
+            st.put(key, i);
+        }
+
+        for (String s : st.levelOrder()) {
+            StdOut.println(s + " " + st.get(s));
+        }
+
+        StdOut.println();
+
+        for (String s : st.keys()) {
+            StdOut.println(s + " " + st.get(s));
+        }
+
+        StdOut.println(st.isBinaryTree());
+
+    }
+}
